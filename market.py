12560f828dd78efa454771e250bd3c2e9355c320
imyashsingh <yash.singh@gmail.com>
2025-07-08 13:23:10 +0530

    Implement Flask routes and templates for home and market pages with Bootstrap styling

--- market.py
+++ market.py
@@ -1,16 +1,25 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app=Flask(__name__)
 
+
+items = [
+        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
+        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
+        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
+    ]
+
 # home page route 
 @app.route('/')
+@app.route('/home')
 def index():
-    return 'Hello, World!'
+    return render_template('home.html')
 
-# about page route
-@app.route('/about/<username>')
-def about(username):
-    return f'This is the about page. {username}'
+# market page route
+@app.route('/market')
+def market():
+    context={ 'items': items}
+    return render_template('market.html',**context)
 
 if __name__ == '__main__':
     app.run(debug=True)
